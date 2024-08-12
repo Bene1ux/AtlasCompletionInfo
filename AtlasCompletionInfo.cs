a2061c556ce5be7b54ed53982fd1f938e2fcdadb
sTafnio <>
2024-08-12 15:31:16 +0200

    added HighlightPartyMembers

--- AtlasCompletionInfo.cs
+++ AtlasCompletionInfo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using ExileCore;
+using ExileCore.PoEMemory.Components;
 using ImGuiNET;
 using SharpDX;
 using Vector2 = System.Numerics.Vector2;
@@ -11,14 +12,14 @@ namespace AtlasCompletionInfo;
 public class AtlasCompletionInfo : BaseSettingsPlugin<AtlasCompletionInfoSettings>
 {
 
-    List<(string, int)> AtlasMaps = new List<(string, int)>();
-    List<(string, int)> AtlasUniqueMaps = new List<(string, int)>();
+    List<(string, int)> AtlasMaps = [];
+    List<(string, int)> AtlasUniqueMaps = [];
     public string[] MapsToExclude;
 
     private string[] CompletedMaps = [];
 
-    List<(string, int)> MissingMaps = new List<(string, int)>();
-    List<(string, int)> MissingUniqueMaps = new List<(string, int)>();
+    List<(string, int)> MissingMaps = [];
+    List<(string, int)> MissingUniqueMaps = [];
 
     private int AmountCompleted = 0;
     private int NewAmountCompleted = 0;
@@ -51,12 +52,13 @@ public class AtlasCompletionInfo : BaseSettingsPlugin<AtlasCompletionInfoSetting
 
         return null;
     }
+
     private void UpdateAtlasMaps()
     {
         var ingameState = GameController.Game.IngameState;
         var atlasNodesAux = ingameState.TheGame.Files.AtlasNodes.EntriesList;
 
-        List<(string, int, int)> AtlasMapsFromAtlasNodes = new List<(string, int, int)>();
+        List<(string, int, int)> AtlasMapsFromAtlasNodes = [];
         foreach (var node in atlasNodesAux)
         {
 
@@ -95,7 +97,6 @@ public class AtlasCompletionInfo : BaseSettingsPlugin<AtlasCompletionInfoSetting
             if (!Settings.SortAlphabetically)
             {
                 MissingMaps = MissingMapsAux.OrderBy(tuple => tuple.Item2).ThenBy(tuple => tuple.Item1).ToList();
-    
                 MissingUniqueMaps = MissingUniqueMapsAux.OrderBy(tuple => tuple.Item2).ThenBy(tuple => tuple.Item1).ToList();
             }
             else
@@ -134,96 +135,125 @@ public class AtlasCompletionInfo : BaseSettingsPlugin<AtlasCompletionInfoSetting
         DebugWindow.LogMsg("Uncompleted Maps copied", 3f);
     }
 
+    private bool ShouldDrawParty()
+    {
+        var anyFullscreenPanelVisible = GameController.IngameState.IngameUi.FullscreenPanels.Any(panel => panel.IsVisible);
+        if (!anyFullscreenPanelVisible &&
+            !GameController.Game.IngameState.IngameUi.OpenLeftPanel.IsVisible &&
+            !GameController.IngameState.IngameUi.ChatTitlePanel.IsVisible)
+        {
+            return true;
+        }
+        return false;
+    }
+
     public override void Render()
     {
-        if (!Settings.ShowMapsOnAtlas || !GameController.IngameState.IngameUi.Atlas.IsVisible || GameController.IngameState.IngameUi.OpenLeftPanel.IsVisible)
+        if (Settings.HighlightPartyMembers &&
+            !GameController.IngameState.IngameUi.FullscreenPanels.Any(panel => panel.IsVisible) &&
+            !GameController.Game.IngameState.IngameUi.OpenLeftPanel.IsVisible &&
+            !GameController.IngameState.IngameUi.ChatTitlePanel.IsVisible)
         {
-            return;
+            var partyPlayers = GameController.IngameState.IngameUi.PartyElement.Children[0].Children[0].Children;
+
+            foreach (var player in partyPlayers)
+            {
+                var areaName = player.Children[2].Text;
+
+                if (MissingMaps.Any(map => map.Item1 == areaName))
+                {
+                    var pos = player.GetClientRect();
+                    Graphics.DrawFrame(pos, new Color(0, 255, 0, 255), 2);
+                }
+            }
         }
 
-        // Header
-        var scrRect = GameController.Window.GetWindowRectangle();
 
-        var headerX = scrRect.Width * 0.05f;
-        var headerY = scrRect.Height * 0.08f;
-        var headerWidth = scrRect.Width * 0.2f;
-        var headerHeight = 20;
-        var headerImage = new RectangleF(headerX, headerY, headerWidth, headerHeight);
-
-        Graphics.DrawImage("preload-start.png", headerImage);
-
-        var headerText = "Uncompleted Maps";
-        var headerTextSize = Graphics.MeasureText(headerText);
-        var headerTextX = headerX + (headerWidth - headerTextSize.X) / 2;
-        var headerTextY = headerY + (headerHeight - headerTextSize.Y) / 2;
-
-        Graphics.DrawText(headerText, new Vector2(headerTextX, headerTextY));
-
-        // Missing Maps
-        var hoverCheck = ImGui.GetMousePos();
-        if (headerImage.Contains(hoverCheck.X, hoverCheck.Y))
+        if (Settings.ShowMapsOnAtlas && GameController.IngameState.IngameUi.Atlas.IsVisible && !GameController.IngameState.IngameUi.OpenLeftPanel.IsVisible)
         {
+            // Header
+            var scrRect = GameController.Window.GetWindowRectangle();
 
-            var entryWidth = 150;
-            var columnSpacing = 10;
-            var rowSpacing = 5;
-            var textPadding = 5;
-            var startY = scrRect.Height * 0.15f;
-            var endY = scrRect.Height * 0.70f;
-            var startX = scrRect.Width * 0.08f;
-            var entryHeight = (int)Math.Floor(scrRect.Height * 0.0167f);
-            var fontSize = ImGui.GetFontSize();
+            var headerX = scrRect.Width * 0.05f;
+            var headerY = scrRect.Height * 0.08f;
+            var headerWidth = scrRect.Width * 0.2f;
+            var headerHeight = 20;
+            var headerImage = new RectangleF(headerX, headerY, headerWidth, headerHeight);
 
-            var maxEntriesPerColumn = (int)Math.Floor((endY - startY) / (entryHeight + rowSpacing));
+            Graphics.DrawImage("preload-start.png", headerImage);
 
+            var headerText = "Uncompleted Maps";
+            var headerTextSize = Graphics.MeasureText(headerText);
+            var headerTextX = headerX + (headerWidth - headerTextSize.X) / 2;
+            var headerTextY = headerY + (headerHeight - headerTextSize.Y) / 2;
 
-            // Regular Maps
-            for (int i = 0; i < AmountMissing; i++)
+            Graphics.DrawText(headerText, new Vector2(headerTextX, headerTextY));
+
+            // Missing Maps
+            var hoverCheck = ImGui.GetMousePos();
+            if (headerImage.Contains(hoverCheck.X, hoverCheck.Y))
             {
-                var columnIndex = i / maxEntriesPerColumn;
-                var rowIndex = i % maxEntriesPerColumn;
 
-                var entryX = startX + columnIndex * (entryWidth + columnSpacing);
-                var entryY = startY + rowIndex * (entryHeight + rowSpacing);
+                var entryWidth = 150;
+                var columnSpacing = 10;
+                var rowSpacing = 5;
+                var textPadding = 5;
+                var startY = scrRect.Height * 0.15f;
+                var endY = scrRect.Height * 0.70f;
+                var startX = scrRect.Width * 0.08f;
+                var entryHeight = (int)Math.Floor(scrRect.Height * 0.0167f);
+                var fontSize = ImGui.GetFontSize();
 
-                var entryText = MissingMaps[i].Item1;
+                var maxEntriesPerColumn = (int)Math.Floor((endY - startY) / (entryHeight + rowSpacing));
 
-                if (Settings.ShowTiers)
+                // Regular Maps
+                for (int i = 0; i < AmountMissing; i++)
                 {
-                    entryText += " " + MissingMaps[i].Item2;
+                    var columnIndex = i / maxEntriesPerColumn;
+                    var rowIndex = i % maxEntriesPerColumn;
+
+                    var entryX = startX + columnIndex * (entryWidth + columnSpacing);
+                    var entryY = startY + rowIndex * (entryHeight + rowSpacing);
+
+                    var entryText = MissingMaps[i].Item1;
+
+                    if (Settings.ShowTiers)
+                    {
+                        entryText += " " + MissingMaps[i].Item2;
+                    }
+
+                    var entryTextX = entryX + textPadding;
+                    var entryTextY = entryY + (entryHeight - fontSize) / 2;
+
+                    Graphics.DrawImage("menu-background.png", new RectangleF(entryX, entryY, entryWidth, entryHeight));
+                    Graphics.DrawText(entryText, new Vector2(entryTextX, entryTextY));
                 }
 
-                var entryTextX = entryX + textPadding;
-                var entryTextY = entryY + (entryHeight - fontSize) / 2;
-
-                Graphics.DrawImage("menu-background.png", new RectangleF(entryX, entryY, entryWidth, entryHeight));
-                Graphics.DrawText(entryText, new Vector2(entryTextX, entryTextY));
-            }
-
-            // Unique Maps
-            var uniqueEntryWidth = 200;
-            var separatorPad = 30;
-            var uniqueMapsStartX = startX + separatorPad + (entryWidth + columnSpacing) * ((MissingMaps.Count + maxEntriesPerColumn - 1) / maxEntriesPerColumn);
-            for (int i = 0; i < AmountUniqueMissing; i++)
-            {
-                var columnIndex = i / maxEntriesPerColumn;
-                var rowIndex = i % maxEntriesPerColumn;
-
-                var entryX = uniqueMapsStartX + columnIndex * (entryWidth + columnSpacing);
-                var entryY = startY + rowIndex * (entryHeight + rowSpacing);
-
-                var entryText = MissingUniqueMaps[i].Item1;
-
-                if (Settings.ShowTiers)
+                // Unique Maps
+                var uniqueEntryWidth = 200;
+                var separatorPad = 30;
+                var uniqueMapsStartX = startX + separatorPad + (entryWidth + columnSpacing) * ((MissingMaps.Count + maxEntriesPerColumn - 1) / maxEntriesPerColumn);
+                for (int i = 0; i < AmountUniqueMissing; i++)
                 {
-                    entryText += " " + MissingUniqueMaps[i].Item2;
+                    var columnIndex = i / maxEntriesPerColumn;
+                    var rowIndex = i % maxEntriesPerColumn;
+
+                    var entryX = uniqueMapsStartX + columnIndex * (entryWidth + columnSpacing);
+                    var entryY = startY + rowIndex * (entryHeight + rowSpacing);
+
+                    var entryText = MissingUniqueMaps[i].Item1;
+
+                    if (Settings.ShowTiers)
+                    {
+                        entryText += " " + MissingUniqueMaps[i].Item2;
+                    }
+
+                    var entryTextX = entryX + textPadding;
+                    var entryTextY = entryY + (entryHeight - fontSize) / 2;
+
+                    Graphics.DrawImage("menu-background.png", new RectangleF(entryX, entryY, uniqueEntryWidth, entryHeight));
+                    Graphics.DrawText(entryText, new Vector2(entryTextX, entryTextY));
                 }
-
-                var entryTextX = entryX + textPadding;
-                var entryTextY = entryY + (entryHeight - fontSize) / 2;
-
-                Graphics.DrawImage("menu-background.png", new RectangleF(entryX, entryY, uniqueEntryWidth, entryHeight));
-                Graphics.DrawText(entryText, new Vector2(entryTextX, entryTextY));
             }
         }
     }
--- AtlasCompletionInfoSettings.cs
+++ AtlasCompletionInfoSettings.cs
@@ -9,22 +9,37 @@ public class AtlasCompletionInfoSettings : ISettings
 {
     public ToggleNode Enable { get; set; } = new ToggleNode(false);
 
-    [Menu("Show Maps on Atlas screen")]
+
+    [Menu("General Settings", 0, CollapsedByDefault = false)]
+    public EmptyNode Empty0 { get; set; }
+
+    [Menu("Show Maps on Atlas screen", parentIndex = 0)]
     public ToggleNode ShowMapsOnAtlas { get; set; } = new ToggleNode(false);
 
-    [Menu("Sort alphabetically")]
-    public ToggleNode SortAlphabetically { get; set; } = new ToggleNode(false);
+    [Menu("Sort missing Maps alphabetically", parentIndex = 0)]
+    public ToggleNode SortAlphabetically { get; set; } = new ToggleNode(true);
 
-    [Menu("Show Base Map Tiers")]
-    public ToggleNode ShowTiers { get; set; } = new ToggleNode(true);
+    [Menu("Show Base Map Tiers", parentIndex = 0)]
+    public ToggleNode ShowTiers { get; set; } = new ToggleNode(false);
 
-    [Menu("Copy uncompleted Maps")]
-    public ButtonNode Copy { get; set; } = new ButtonNode();
+    [Menu("Highlight Party Members in uncompleted Maps", parentIndex = 0)]
+    public ToggleNode HighlightPartyMembers { get; set; } = new ToggleNode(false);
 
-    [Menu("Hotkey to Copy:")]
+
+    [Menu("Copy Settings", 1, CollapsedByDefault = false)]
+    public EmptyNode Empty1 { get; set; }
+
+    [Menu("Include Base Map Tiers", parentIndex = 1)]
+    public ToggleNode IncludeBaseTiers { get; set; } = new ToggleNode(false);
+
+    [Menu("Hotkey to Copy:", parentIndex = 1)]
     public HotkeyNode CopyHotkey { get; set; } = Keys.None;
 
-    [Menu("Include Base Map Tiers")]
-    public ToggleNode IncludeBaseTiers { get; set; } = new ToggleNode(false);
+    [Menu("Copy uncompleted Maps", parentIndex = 1)]
+    public ButtonNode Copy { get; set; } = new ButtonNode();
+
+
+
+
 
 }
